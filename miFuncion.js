818fbee375ec34e1b7acfe93f66e2c945acd484b
martinpaz361 <123372308+martinpaz361@users.noreply.github.com>
2023-03-25 19:10:01 -0300

    Add files via upload

---
+++ miFuncion.js
@@ -0,0 +1,41 @@
+function miFuncion0() {
+    let x = document.getElementById("main0");
+    
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+    }  
+  }
+  
+  function miFuncion1() {
+    let x = document.getElementById("main1");
+    
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+  }
+  
+  
+  function miFuncion2(){
+  let x = document.getElementById("main2")
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      
+    } else {
+      x.style.display = "none";
+  }
+  }
+  
+  
+  function miFuncion3(){
+    let x = document.getElementById("main3")
+      if (x.style.display === "none") {
+        x.style.display = "block";
+        
+      } else {
+        x.style.display = "none";
+    }
+    }
